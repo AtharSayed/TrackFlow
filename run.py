369656571aecea293c7202a0aa5db50c4e4421e2
Athar Sayed <sayedathar242@gmail.com>
2025-03-18 00:40:19 +0530

    Added Cv and pyzbar libs for implmn

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
 # Entry point to run the Flask app
+import cv2
+import numpy as np
+import qrcode
+import json
+import pymongo
+from flask import Flask, request, jsonify, render_template
+import pyzbar.pyzbar as pyzbar
+import os
+
 from app import app
 
 if __name__ == '__main__':
